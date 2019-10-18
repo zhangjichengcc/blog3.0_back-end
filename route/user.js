65f1e00f4fa91bd14e277e18ad92f1c38769b017
zhangjc <zhangjc@hua-cloud.com.cn>
2019-10-18 18:44:50 +0800

    update

---
+++ route/user.js
@@ -0,0 +1,15 @@
+const app = require('../app.js');
+
+const {
+  login,
+} = require('../service/user.js');
+
+// 上传图片
+app.get('/api/user/login', (req, res) => { login(req, res) });
+
+const userServer = app.listen(5003, () => {
+  const host = userServer.address().address
+  const port = userServer.address().port
+	console.log('server: user');
+	console.log('listen: http://%s:%s', host, port);
+});
