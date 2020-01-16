7add25ce525359d581897340c73ff00237dcf033
zhangjc <zhangjc@hua-cloud.com.cn>
2020-01-16 16:33:10 +0800

    update

--- service/artical.js
+++ service/artical.js
@@ -1,3 +1,11 @@
+/*
+ * @Author: zhang jicheng
+ * @Date: 2019-08-15 16:57:18
+ * @LastEditTime : 2020-01-04 14:40:23
+ * @LastEditors  : Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \node\service\artical.js
+ */
 const BLOGDB = require('../database.js');
 
 // 查询文章列表
@@ -5,14 +13,14 @@ const selectArticalList = (req, res) => {
 	const { pageNum = 1, pageSize = 10 } = req.query;
 	// 定义SQL语句
 	// const sqlStr = `SELECT a.id, a.title, a.introduction, a.banner, a.create_time createTime, a.read_count readCount, a.like_count likeCount, msgCount, type FROM artical a LEFT JOIN ( SELECT count(*) msgCount, m.artical_id FROM artical_message m GROUP BY artical_id ) t ON a.id = t.artical_id LEFT JOIN ( SELECT ty.name type, ty.artical_id FROM artical_type ty ) t2 ON a.id = t2.artical_id LIMIT ${(pageNum - 1) * pageSize}, ${pageNum * pageSize}; SELECT count(*) total FROM artical;`;
-	const sqlStr = `SELECT a.id, a.title, a.introduction, a.banner, DATE_FORMAT(a.create_time,"%Y-%m-%d %H:%i:%s") createTime, a.read_count readCount, a.like_count likeCount, msgCount, type \
+	const sqlStr = `SELECT a.id, a.title, a.publish, a.introduction, a.banner, DATE_FORMAT(a.create_time,"%Y-%m-%d %H:%i:%s") createTime, a.read_count readCount, a.like_count likeCount, msgCount, type \
 	FROM artical a \
 	LEFT JOIN ( SELECT count(*) msgCount, m.artical_id FROM artical_message m GROUP BY artical_id ) t \
 	ON a.id = t.artical_id \
 	LEFT JOIN ( SELECT ty.name type, ty.artical_id FROM artical_type ty ) t2 \
 	ON a.id = t2.artical_id \
 	ORDER BY create_time DESC \
-	LIMIT ${(pageNum - 1) * pageSize}, ${pageNum * pageSize}; \
+	LIMIT ${pageNum - 1}, ${pageSize}; \
 	SELECT count(*) total FROM artical; \
 	`
 	BLOGDB.query(sqlStr, (err, results) => {
@@ -31,9 +39,9 @@ const selectArticalList = (req, res) => {
 
 // 插入文章
 const insertArtical = (req, res) => {
-	const { title = null, introduction = '文章简介', banner = null, mainContent = null, createTime =  new Date(), readCount = 0 } = req.body;
-	const sqlStr = `INSERT INTO artical (title, introduction, banner, main_content, create_time, read_count) VALUES (?, ?, ?, ?, ?, ?);`
-	BLOGDB.query(sqlStr, [title, introduction, banner, mainContent, createTime, readCount], (err, results) => {
+	const { title = null, introduction = '文章简介', publish = false, banner = null, mainContent = null, createTime =  new Date(), readCount = 0 } = req.body;
+	const sqlStr = `INSERT INTO artical (title, introduction, publish, banner, main_content, create_time, read_count) VALUES (?, ?, ?, ?, ?, ?, ?);`
+	BLOGDB.query(sqlStr, [title, introduction, publish, banner, mainContent, createTime, readCount], (err, results) => {
 		console.log('errormsg: ', err, 'results:', results);
 		if(err) return res.json({code: -1, message: '获取失败'});
 		const resData = {id: results.insertId};
@@ -47,10 +55,10 @@ const insertArtical = (req, res) => {
 
 // 更新文章
 const updateArtical = (req, res) => {
-	const { id, title = null, introduction = '文章简介', banner = null, mainContent = null, createTime = new Date() } = req.body;
+	const { id, title = null, publish = false, introduction = '文章简介', banner = null, mainContent = null, createTime = new Date() } = req.body;
 	// const sqlStr = `UPDATE artical SET title=${title}, banner=${banner}, create_time=${createTime}, introduction=${introduction}, main_content=${mainContent} WHERE id=${id};`
-	const sqlStr = `UPDATE artical SET title=?, banner=?, create_time=?, introduction=?, main_content=? WHERE id=?;`
-	BLOGDB.query(sqlStr, [title, banner, createTime, introduction,mainContent, id], (err, results) => {
+	const sqlStr = `UPDATE artical SET title=?, publish=?, banner=?, create_time=?, introduction=?, main_content=? WHERE id=?;`
+	BLOGDB.query(sqlStr, [title, publish, banner, createTime, introduction, mainContent, id], (err, results) => {
 		console.log('errormsg: ', err, 'results:', results);
 		if(err) return res.json({code: -1, message: '更新失败'});
 		const resData = {id};
@@ -65,7 +73,7 @@ const updateArtical = (req, res) => {
 // 根据id查找文章
 const selectArtical = (req, res) => {
 	const { id } = req.query;
-	const sqlStr = `SELECT id, title, introduction, banner, DATE_FORMAT(create_time,"%Y-%m-%d %H:%i:%s") createTime, read_count readCount, like_count likeCount, main_content mainContent FROM artical WHERE id = ${id};`
+	const sqlStr = `SELECT id, title, introduction, publish, banner, DATE_FORMAT(create_time,"%Y-%m-%d %H:%i:%s") createTime, read_count readCount, like_count likeCount, main_content mainContent FROM artical WHERE id = ${id};`
 	BLOGDB.query(sqlStr, (err, results) => {
 		console.log('errormsg: ', err, 'results:', results);
 		if(err) return res.json({code: -1, message: '获取失败'});
